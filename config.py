e70d2b4f940fc086c1dbab19bb6462918d6ff041
Himank Jog <himank.juit@gmail.com>
2017-03-14 19:34:04 +0530

    fixed KeyError: 'DATABASE_URL'

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ aplhanumeric_set = "".join([choice(char_set) for _ in range(randint(1,1000))])
 class BaseConfig(object):
 	DEBUG = False
 	SECRET_KEY = '\x89\xf2\xea\xc9\x11\xab\xda\xaf\x0c-)w\xf9AJ<U\x04Dh\x8112'
-	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL ']
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class DevelopmentConfig(BaseConfig):
